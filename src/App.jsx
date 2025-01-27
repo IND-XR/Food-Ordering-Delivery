447f08548060f8e1d94c6871c935231160ee0613
insta <anmolpreetsingh510@gmail.com>
2025-01-27 07:31:57 +0530

    Create Home pages Routes (Home ,cart , order )  complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,21 @@
 import React from 'react'
 import Navbar from './components/Navbar/Navbar'
+import { Route, Routes } from 'react-router-dom'
+import Home from './pages/Home/Home'
+import PlaceOrder from './pages/placeOrder/PlaceOrder'
+
 
 const App = () => {
   return (
     <div className='app'>
       <Navbar/>
+      <Routes>   {/* creating Routes for all the pages */}
+        <Route path='/' element={<Home/>}/>
+        <Route path='/Cart' element={<Cart/>}/>  
+        <Route path='/order' element={<PlaceOrder/>}/>
+        
+  
+      </Routes>
     </div>
   )
 }
